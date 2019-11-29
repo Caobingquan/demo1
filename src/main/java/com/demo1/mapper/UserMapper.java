9710d0ebb6c15fd705f3ed1110d8d89f2d6359d5
Cao <Binquan.Cao@connext.com.cn>
2019-11-29 16:50:34 +0800

    first push

---
+++ src/main/java/com/demo1/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.demo1.mapper;
+
+import com.demo1.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends tk.mybatis.mapper.common.Mapper<User>{
+}
---
+++ src/main/java/com/demo1/service/UserService.java
@@ -0,0 +1,18 @@
+package com.demo1.service;
+
+import com.demo1.mapper.UserMapper;
+import com.demo1.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id) {
+        return this.userMapper.selectByPrimaryKey(id);
+    }
+}
+
